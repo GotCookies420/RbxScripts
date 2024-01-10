be414e3c623727ca7551e9ce2f65963e4fd553f1
GotCookies420 <156153479+GotCookies420@users.noreply.github.com>
2024-01-10 08:12:03 -0600

    Wiped ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,17 +1,3 @@
-local plrService = game.Players
+-- rewriting LuaU
 
-function newESP(char)
-local esphl = Instance.new("Highlight")
-
-esphl.Parent = char
-end)
-
-for _, p in pairs(plrService:GetPlayers()) do
-newESP(p.Character)
-end
-
-plrService.PlayerAdded:Connect(function(p)
-  p.CharacterAdded:Connect(function(c)
-      newESP(c)
-    end)
-end)
+-- sorry.
