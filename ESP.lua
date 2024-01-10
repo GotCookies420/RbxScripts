aceaed72f56f05422de7fb540766814b013b6ee6
GotCookies420 <156153479+GotCookies420@users.noreply.github.com>
2024-01-10 13:36:28 -0600

    Update ESP.lua (again)

--- ESP.lua
+++ ESP.lua
@@ -1,40 +1,53 @@
 local p = game.Players.LocalPlayer
 local life = 1
 local extra = .1
+local espItems = {}
 
-function refreshESP(target:Player)
-	local highlight = Instance.new("Highlight")
-	local billboard = Instance.new("BillboardGui")
-	local textlabel = Instance.new("TextLabel")
-
-	highlight.Parent = target.Character
-	highlight.OutlineTransparency = 0
-	
-	if target.Neutral == false then
-		highlight.FillColor = target.Team.TeamColor.Color
-		highlight.OutlineColor = target.Team.TeamColor.Color
-	else
-		highlight.FillColor = Color3.fromRGB(143, 143, 143)
-		highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+function refreshESP()
+	for _, EI in pairs(espItems) do
+		EI:Destroy()
 	end
 	
-	billboard.Adornee = target.Character
-	billboard.Parent = p.PlayerGui
+	espItems = {}
 	
-	textlabel.Visible = true
-	textlabel.BackgroundTransparency = 1
-	textlabel.Parent = billboard
-	textlabel.Size = UDim2.new({1, 0}, {1, 0})
-	textlabel.TextScaled = true
-	textlabel.Font = Enum.Font.ArialBold
-	textlabel.Text = target.Name.." ("..target.Character:FindFirstChildOfClass("Humanoid").Health.." Health)"
+	for _, target in pairs(game.Players:GetPlayers()) do
+		local highlight = Instance.new("Highlight")
+		local billboard = Instance.new("BillboardGui")
+		local textlabel = Instance.new("TextLabel")
 
-	game.Debris:AddItem(highlight, life + extra)
-	game.Debris:AddItem(billboard, life + extra)
-end
+		highlight.Parent = target.Character
+		highlight.OutlineTransparency = 0
 
-while task.wait(life) do
-	for _, enemy in pairs(game.Players:GetPlayers()) do
-		refreshESP(enemy)
+		if target.Neutral == false then
+			highlight.FillColor = target.Team.TeamColor.Color
+			highlight.OutlineColor = target.Team.TeamColor.Color
+		else
+			highlight.FillColor = Color3.fromRGB(143, 143, 143)
+			highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+		end
+
+		billboard.Adornee = target.Character
+		billboard.Parent = p.PlayerGui
+
+		textlabel.Visible = true
+		textlabel.BackgroundTransparency = 1
+		textlabel.Parent = billboard
+		textlabel.Size = UDim2.new({1, 0}, {1, 0})
+		textlabel.TextScaled = true
+		textlabel.Font = Enum.Font.ArialBold
+		textlabel.Text = target.Name.." ("..target.Character:FindFirstChildOfClass("Humanoid").Health.." Health)"
+		
+		table.insert(espItems, highlight)
+		table.insert(espItems, billboard)
 	end
 end
+
+game.Players.PlayerAdded:Connect(function(p2)
+	p2.CharacterAdded:Connect(function()
+		refreshESP()
+	end)
+end)
+
+game.Players.PlayerRemoving:Connect(function()
+	refreshESP()
+end)
