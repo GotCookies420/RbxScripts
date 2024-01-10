51f8b5484396069ff96aa974b7d869904226b4ca
GotCookies420 <156153479+GotCookies420@users.noreply.github.com>
2024-01-10 08:27:36 -0600

    Rewrote ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,3 +1,33 @@
--- rewriting LuaU
+local p = game.Players.LocalPlayer
 
--- sorry.
+function refreshESP(target:Player)
+	local highlight = Instance.new("Highlight")
+	local billboard = Instance.new("BillboardGui")
+	local textlabel = Instance.new("TextLabel")
+	local life = 0.01
+	
+	highlight.Parent = target.Character
+	billboard.Parent = target.Character
+	billboard.Size = UDim2.new({10, 0},{2, 0})
+	billboard.StudsOffsetWorldSpace = Vector3.new(0, 5, 0)
+	billboard.LightInfluence = 0
+	billboard.AlwaysOnTop = true
+	billboard.ResetOnSpawn = false
+	textlabel.Parent = billboard
+	textlabel.Size = UDim2.new({1, 0}, {1, 0})
+	textlabel.TextScaled = true
+	textlabel.Font = Enum.Font.ArialBold
+	textlabel.Text = target.Name
+	
+	game.Debris:AddItem(highlight, life)
+	game.Debris:AddItem(billboard, life)
+	game.Debris:AddItem(textlabel, life)
+end
+
+game["Run Service"].RenderStepped:Connect(function()
+	for _, enemy in pairs(game.Players:GetPlayers()) do
+		if enemy ~= p then
+			refreshESP(enemy)
+		end
+	end
+end)
