5251081c82326751b92e1bdc2158bbc5cbb6d9e0
GotCookies420 <156153479+GotCookies420@users.noreply.github.com>
2024-01-10 13:21:40 -0600

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -5,23 +5,36 @@ function refreshESP(target:Player)
 	local highlight = Instance.new("Highlight")
 	local billboard = Instance.new("BillboardGui")
 	local textlabel = Instance.new("TextLabel")
-	
+
 	highlight.Parent = target.Character
-	billboard.Parent = target.Character
-	billboard.Size = UDim2.new({10, 0},{2, 0})
-	billboard.StudsOffsetWorldSpace = Vector3.new(0, 5, 0)
-	billboard.LightInfluence = 0
-	billboard.AlwaysOnTop = true
-	billboard.ResetOnSpawn = false
+	
+	if target.Neutral == false then
+		highlight.FillColor = target.Team.TeamColor.Color
+		highlight.OutlineColor = target.Team.TeamColor.Color
+	else
+		highlight.FillColor = Color3.fromRGB(143, 143, 143)
+		highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
+	end
+	
+	billboard.Adornee = target.Character
+	billboard.Parent = p.PlayerGui
+	
+	textlabel.Visible = true
+	textlabel.BackgroundTransparency = 1
 	textlabel.Parent = billboard
 	textlabel.Size = UDim2.new({1, 0}, {1, 0})
 	textlabel.TextScaled = true
 	textlabel.Font = Enum.Font.ArialBold
-	textlabel.Text = target.Name
-	
+	textlabel.Text = target.Name.." ("..target.Character:FindFirstChildOfClass("Humanoid").Health.." Health)"
+
 	game.Debris:AddItem(highlight, life)
 	game.Debris:AddItem(billboard, life)
-	game.Debris:AddItem(textlabel, life)
+end
+
+while task.wait(life) do
+	for _, enemy in pairs(game.Players:GetPlayers()) do
+		refreshESP(enemy)
+	end
 end
 
 while task.wait(life) do
